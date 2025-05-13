23cc6c0bab283af3c2d7480732612de4905c165e
trapala85mx <rtrapala.dev.ds@gmail.com>
2025-05-13 11:49:51 -0600

    [UPDATE] gitignore to igorne media/* [ADD] web/models.py reigstered un admin in a simply

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
+"""web/admin.py"""
+
 from django.contrib import admin
 
+from .models import Categorie, Product
+
 # Register your models here.
+admin.site.register(Categorie)
+admin.site.register(Product)
